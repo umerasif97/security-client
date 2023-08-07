b36971c447747d66f118ecc2c47931b0c1100e37
Mirzet Zukic <zukicmirzet94@gmail.com>
2023-08-07 15:46:12 +0200

    Fix minor bugs

--- source/model/commonUserCreate.ts
+++ source/model/commonUserCreate.ts
@@ -1,7 +1,6 @@
-export interface CommonUserCreate {
-    name?: string;
-    description?: string;
-    tenantId?: string;
+import { SecurityUserCreate } from "./securityUserCreate";
+
+export interface CommonUserCreate extends SecurityUserCreate {
     email?: string;
     lastName?: string;
     password?: string;
--- source/model/tenantToBaseclass.ts
+++ source/model/tenantToBaseclass.ts
@@ -1,8 +1,6 @@
-import { SecurityEntity } from './securityEntity';
 import { SecurityLink } from './securityLink';
 import { SecurityTenant } from './securityTenant';
 
 export interface TenantToBaseclass extends SecurityLink {
     tenant?: SecurityTenant;
-    securityEntity?: SecurityEntity;
 }
